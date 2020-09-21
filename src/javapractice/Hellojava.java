f7168bde3ffe707bcf55d7eb1aa6d9cc1bc6b82f
AV-NASH <avinash21213434@gmail.com>
2020-09-21 09:37:45 +0530

    Initial commit

---
+++ src/javapractice/Hellojava.java
@@ -0,0 +1,21 @@
+package javapractice;
+
+
+import java.util.Scanner;
+
+public class Hellojava {
+	public static void main(String[] arg) {
+		Scanner sc=new Scanner(System.in);
+		System.out.println("Enter the two numbers");
+		int a=sc.nextInt();
+		int b=sc.nextInt();
+		add(a,b);
+	
+	}
+	
+	private static void add(int num1,int num2) {
+		long sum=num1+num2;
+		
+	}
+}
+
